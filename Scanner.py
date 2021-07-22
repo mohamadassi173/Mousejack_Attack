a6e76b04aa93404f54b530caa8aa8c90ecb37104
mohamadassi <mhmdassi@hotmail.com>
2021-07-22 15:26:05 +0300

    modified scanner

--- Scanner.py
+++ Scanner.py
@@ -7,57 +7,74 @@ import nrf24
 
 def getCompanyName(payload):
 
-    print(len(payload))
     # logitech mouse movement packet
-    if len(payload) == 29 and payload[0] == '0' and payload[3:5] == 'C2':
+    if len(payload) == 10 and payload[0] == '0' and payload[1] == 0xC2:
         return 'logitech Mouse'
 
     # logitech keystroke packet
-    elif len(payload) == 22 and payload[0] == 0 and payload[3:5] == 'D3':
+    elif len(payload) == 22 and payload[0] == 0 and payload[1] == 0xD3:
         return 'logitech Keyboard or Mouse'
 
     # logitech keepalive packet
-    elif len(payload) == 5 and payload[0] == 0 and payload[3:5] == '40':
+    elif len(payload) == 5 and payload[0] == 0 and payload[1] == 0x40:
         return 'logitech Keyboard or Mouse'
 
     # logitech sleep timer packet
-    elif len(payload) == 10 and payload[0] == 0 and payload[3:5] == '4F':
+    elif len(payload) == 10 and payload[0] == 0 and payload[1] == 0x4F:
         return 'logitech Keyboard or Mouse'
 
+    # non-XOR encrypted Microsoft mouse
+    elif len(payload) == 19 and (payload[0] == 0x08 or payload[0] == 0x0c) and payload[6] == 0x40:
+        return "microsoft mouse or keyboard"
 
-def scan(timeout=120.0):
+    # XOR encrypted Microsoft mouse
+    elif len(payload) == 19 and payload[0] == 0x0a:
+        return "microsoft mouse or keyboard"
+
+    elif len(payload) == 6:
+        return "amazon mouse or keyboard"
+
+
+def scan(timeout=20):
+
+    # init the radio
     channel_index = 0
     channels = range(2, 84)
     devices = {}
-    ping = [0x0f, 0x0f, 0x0f, 0x0f]
     radio = nrf24.nrf24(0)
     radio.enter_promiscuous_mode()
     radio.set_channel(channels[channel_index])
+
+    # set timing
     dwell_time = 0.1
     last_tune = time.time()
     start_time = time.time()
     radio.enable_lna()
+
+    # start scanning
     while time.time() - start_time < timeout:
-        if len(channels) > 1 and time.time() - last_tune > dwell_time:
+
+        # change the channel
+        if time.time() - last_tune > dwell_time:
             radio.set_channel(channels[channel_index])
             channel_index = (channel_index + 1) % (len(channels))
             last_tune = time.time()
         try:
+            # search for payloads in specific channel
             value = radio.receive_payload()
         except RuntimeError:
             value = []
+
+        # if found something(payload), 5 = mac address length
         if len(value) >= 5:
             address, payload = value[0:5], value[5:]
             address = ':'.join('{:02X}'.format(x) for x in address)
             payload = ':'.join('{:02X}'.format(x) for x in payload)
-            print("address: ", address)
-            print("payload", payload)
-            print("hid", getCompanyName(payload))
             devices[address] = []
             devices[address].append(address)
             devices[address].append(payload)
-
             devices[address].append(getCompanyName(payload))
-
+    # print(devices)
+    return devices
 
 scan()
